17df60359be37d59abfcc408a73971e67994f043
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-03 09:46:30 +0300

    add verbose_name and verbose_name_plural for City model

--- airport/models.py
+++ airport/models.py
@@ -43,6 +43,10 @@ class City(models.Model):
     name = models.CharField(max_length=255)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name = "city"
+        verbose_name_plural = "cities"
+
     def __str__(self):
         return f"{self.name}, {self.country}"
 
