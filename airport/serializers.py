44d256d5b567353ea94128f85f786f300bfb898f
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-03 12:39:24 +0300

    implement AirplaneSerializer

--- airport/serializers.py
+++ airport/serializers.py
@@ -7,3 +7,9 @@ class AirplaneTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = AirplaneType
         fields = "__all__"
+
+
+class AirplaneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AirplaneType
+        fields = ("id", "name", "rows", "seats_in_row", "airplane_type", "capacity")
