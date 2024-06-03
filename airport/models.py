0e4a45499506e511828b887316b65ecd920ee93f
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-03 09:17:21 +0300

    implement Country, City & Airport models

--- airport/models.py
+++ airport/models.py
@@ -28,3 +28,26 @@ class Crew(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=255)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}, {self.country}"
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255)
+    closest_big_city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} ({self.closest_big_city})"
