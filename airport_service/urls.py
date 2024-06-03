0a28807e7f72e9607c18ee5b7263faccdafbc4a0
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-03 09:01:50 +0300

    add airport & user URLs configuration to project

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/airport/", include("airport.urls", namespace="airport")),
+    path("api/user/", include("user.urls", namespace="user")),
 ]
---
+++ user/urls.py
@@ -0,0 +1,3 @@
+urlpatterns = []
+
+app_name = "user"
