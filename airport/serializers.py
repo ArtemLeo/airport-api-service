77d13adb447159a23bdeaea753a76569fd8d4378
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-03 23:56:23 +0300

    implement FlightSerializer, FlightListSerializer & FlightDetailSerializer

--- airport/serializers.py
+++ airport/serializers.py
@@ -7,7 +7,8 @@ from airport.models import (
     Country,
     City,
     Airport,
-    Route
+    Route,
+    Flight
 )
 
 
@@ -89,3 +90,19 @@ class RouteListSerializer(RouteSerializer):
 class RouteDetailSerializer(RouteSerializer):
     source = AirportListSerializer(many=False, read_only=True)
     destination = AirportListSerializer(many=False, read_only=True)
+
+
+class FlightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flight
+        fields = "__all__"
+
+
+class FlightListSerializer(FlightSerializer):
+    route = serializers.StringRelatedField(many=False, read_only=True)
+    airplane = serializers.StringRelatedField(many=False, read_only=True)
+    crew = serializers.StringRelatedField(many=True, read_only=True)
+
+
+class FlightDetailSerializer(FlightListSerializer):
+    route = RouteListSerializer(many=False, read_only=True)
