d342b1420c0f55e1cd66ae82eb59317e734124bc
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-03 07:05:59 +0300

    refactor code with black

--- airport/apps.py
+++ airport/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class AirportConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'airport'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "airport"
