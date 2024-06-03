5f1a96889fbd786ce7d1a7cf494d3db598f154b7
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-03 23:25:17 +0300

    router register airports

--- airport/urls.py
+++ airport/urls.py
@@ -4,7 +4,8 @@ from airport.views import (
     AirplaneViewSet,
     CrewViewSet,
     CountryViewSet,
-    CityViewSet
+    CityViewSet,
+    AirportViewSet
 )
 
 router = routers.DefaultRouter()
@@ -13,6 +14,7 @@ router.register("airplanes", AirplaneViewSet)
 router.register("crews", CrewViewSet)
 router.register("countries", CountryViewSet)
 router.register("cities", CityViewSet)
+router.register("airports", AirportViewSet)
 
 urlpatterns = router.urls
 
