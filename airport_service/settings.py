294a5726d7cfccb284b40f6be9de4bd818613225
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-05 07:06:21 +0300

    add DJANGO_DEBUG env variable

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DJANGO_DEBUG", default=0))
 
 ALLOWED_HOSTS = []
 
