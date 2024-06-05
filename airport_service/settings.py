61ca99258a880d6ff5cc275eb7baf10bcf576129
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-05 06:57:05 +0300

    add DEFAULT_THROTTLE_CLASSES & DEFAULT_THROTTLE_RATES

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -131,6 +131,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "10/minute", "user": "30/minute"},
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
