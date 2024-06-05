9319b533229536cfaf627044b7bdbc2d1aea8694
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-05 15:42:13 +0300

    implement test_list_flights

--- airport/tests/test_flight_api.py
+++ airport/tests/test_flight_api.py
@@ -1,7 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
+
+from airport.models import Flight
+from airport.serializers import FlightListSerializer
+from airport.tests.sample_data import sample_flight
 
 FLIGHT_URL = reverse("airport:flight-list")
 
@@ -17,3 +22,28 @@ class UnauthenticatedFlightApiTests(TestCase):
     def test_auth_required(self):
         res = self.client.get(FLIGHT_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthenticatedFlightApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            "test@test.com",
+            "testpass",
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_list_flights(self):
+        sample_flight()
+        sample_flight()
+
+        res = self.client.get(FLIGHT_URL)
+
+        flights = Flight.objects.all()
+        serializer = FlightListSerializer(flights, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        for index, serializer_object in enumerate(serializer.data):
+            for key in serializer_object:
+                self.assertEqual(serializer_object[key], res.data[index][key])
