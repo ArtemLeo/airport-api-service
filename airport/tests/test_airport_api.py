3111131f3989ba2154d35ea79b7c71798d1ff5b7
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-05 15:11:35 +0300

    implement test_list_airports

--- airport/tests/test_airport_api.py
+++ airport/tests/test_airport_api.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
@@ -13,6 +14,7 @@ from airport.models import (
     Crew,
     Flight
 )
+from airport.serializers import AirportListSerializer
 
 AIRPLANE_URL = reverse("airport:airplane-list")
 AIRPORT_URL = reverse("airport:airport-list")
@@ -100,3 +102,25 @@ class UnauthenticatedAirportApiTests(TestCase):
     def test_auth_required(self):
         res = self.client.get(AIRPORT_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthenticatedAirportApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            "test@test.com",
+            "testpass",
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_list_airports(self):
+        sample_airport()
+        sample_airport()
+
+        res = self.client.get(AIRPORT_URL)
+
+        airports = Airport.objects.all()
+        serializer = AirportListSerializer(airports, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
