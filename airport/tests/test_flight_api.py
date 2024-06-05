9d25944d1d99204668e1272be078daca02a4aa7d
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-05 15:50:15 +0300

    implement test_filter_flights_by_crews

--- airport/tests/test_flight_api.py
+++ airport/tests/test_flight_api.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from airport.models import Flight
+from airport.models import Flight, Crew
 from airport.serializers import FlightListSerializer
 from airport.tests.sample_data import sample_flight, sample_route, sample_airplane
 
@@ -80,6 +80,40 @@ class AuthenticatedFlightApiTests(TestCase):
 
         res = self.client.get(
             FLIGHT_URL, {"airplanes": f"{airplane1.id}, {airplane2.id}"}
+        )
+
+        serializer1 = FlightListSerializer(flight1)
+        serializer2 = FlightListSerializer(flight2)
+        serializer3 = FlightListSerializer(flight3)
+
+        self.assertIn(serializer1.data, res.data)
+        self.assertIn(serializer2.data, res.data)
+        self.assertNotIn(serializer3.data, res.data)
+
+    def test_filter_flights_by_crews(self):
+        crew1 = Crew.objects.create(
+            first_name="First",
+            last_name="Crew"
+        )
+        crew2 = Crew.objects.create(
+            first_name="Second",
+            last_name="Crew"
+        )
+        crew3 = Crew.objects.create(
+            first_name="Third",
+            last_name="Crew"
+        )
+
+        flight1 = sample_flight()
+        flight2 = sample_flight()
+        flight3 = sample_flight()
+
+        flight1.crew.add(crew1)
+        flight2.crew.add(crew2)
+        flight3.crew.add(crew3)
+
+        res = self.client.get(
+            FLIGHT_URL, {"crews": f"{crew1.id}, {crew2.id}"}
         )
 
         serializer1 = FlightListSerializer(flight1)
