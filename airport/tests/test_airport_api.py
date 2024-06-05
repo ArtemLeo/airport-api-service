f34731b19135bc07a90c4b4761edbc303a1308f4
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-05 15:09:42 +0300

    implement UnauthenticatedAirportApiTests

--- airport/tests/test_airport_api.py
+++ airport/tests/test_airport_api.py
@@ -1,4 +1,7 @@
+from django.test import TestCase
 from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
 
 from airport.models import (
     Airplane,
@@ -88,3 +91,12 @@ def sample_flight(**params):
     defaults.update(params)
 
     return Flight.objects.create(**defaults)
+
+
+class UnauthenticatedAirportApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        res = self.client.get(AIRPORT_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
